a3a3d2ec65af32c873dc36d1d75ff063fbb8db31
chelsea-nie <nieyunshuang@126.com>
2016-09-14 14:55:42 +0800

    init

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -ex
+
+echo "${MYID:-1}" > /tmp/zookeeper/myid
+
+# based on https://github.com/apache/zookeeper/blob/trunk/conf/zoo_sample.cfg
+cat > /opt/zookeeper/conf/zoo.cfg <<EOF
+tickTime=2000
+initLimit=10
+syncLimit=5
+dataDir=/tmp/zookeeper
+clientPort=${CLIENTPORT}
+EOF
+
+
+#clientPort=2181
+
+
+# server.1=...
+if [ -n "$SERVERS" ]; then
+    printf '%s' "$SERVERS" | awk 'BEGIN { RS = "," }; { printf "server.%i=%s:2888:3888\n", NR, $0 }' >> /opt/zookeeper/conf/zoo.cfg
+fi
+
+exec "$@"
